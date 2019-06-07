a3ed161362f73e9cec97e52a7a72babe71edc786
Patrick Utz <utznpatrick>
2019-06-07 19:47:25 -0400

    finished beta version of extractor.py that goes up to downloading dataset

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tableau-twitter-analytics
+
+## Description
 A python script designed to extract data from the Twitter data analytics page and pipe that into Tableau for use on desktop and mobile. Data is then available to play around with!
+
+## Installation
+It is recommended to use Anaconda to create a virtual environement and install Python for this. 
--- src/extractor.py
+++ src/extractor.py
@@ -1,10 +1,31 @@
 from selenium import webdriver
+import time
+import keyboard
 
 # Using FireFox to access web and need to set path to driver
-driver = webdriver.Firefox("/Users/patrickutz/CS Projects/python/tableau-twitter-analytics/drivers/geckodriver")
+driver = webdriver.Firefox(executable_path="/Users/patrickutz/cs-projects/python/tableau-twitter-analytics/drivers/geckodriver")
 
 # Open the website
-driver.get("https://twitter.com")
+driver.get("https://analytics.twitter.com/user/WashAbstract/tweets") 
 
-title = driver.title()
-print(title)
+id_box = driver.find_element_by_class_name("js-username-field.email-input.js-initial-focus")
+pass_box = driver.find_element_by_class_name("js-password-field")
+login = driver.find_element_by_class_name("submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium")
+
+# wait for page to load
+time.sleep(4)
+
+# send id and password info
+id_box.click()
+id_box.send_keys("")
+pass_box.click()
+pass_box.send_keys("")
+login.click()
+
+time.sleep(3)
+export_data = driver.find_element_by_class_name("btn.btn-default.ladda-button")
+export_data.click()
+
+# wait for export data to be prepared to download
+time.sleep(10)
+keyboard.press_and_release('enter')
